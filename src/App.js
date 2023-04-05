22b907099076d04c4320a734e17a4c5b577b99c7
mendietr <mendietr1978@icloud.com>
2023-04-05 10:20:19 -0600

    ejercicio 6

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import './App.css';
-import Card from './components/Card.jsx';
 import Cards from './components/Cards.jsx';
-import SearchBar from './components/SearchBar.jsx';
-import characters, { Rick } from './data.js';
+import Nav from './components/Nav';
+import {useState} from 'react';
+
+const example = {
+   id: 1,
+   name: 'Rick Sanchez',
+   status: 'Alive',
+   species: 'Human',
+   gender: 'Male',
+   origin: {
+      name: 'Earth (C-137)',
+      url: 'https://rickandmortyapi.com/api/location/1',
+   },
+   image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+};
+
 
 function App() {
+      const [characters, setCharacters] = useState([]);
+
+      const onSearch = () => {
+         setCharacters([...characters, example])
+      }
+      console.log(characters)
    return (
       <div className='App'>
-         <SearchBar onSearch={(characterID) => window.alert(characterID)} />
+         <Nav onSearch={onSearch}/>
          <Cards characters={characters} />
-         <Card
-            id={Rick.id}
-            name={Rick.name}
-            status={Rick.status}
-            species={Rick.species}
-            gender={Rick.gender}
-            origin={Rick.origin.name}
-            image={Rick.image}
-            onClose={() => window.alert('Emulamos que se cierra la card')}
-         />
+         
       </div>
    );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,19 @@
-function SearchBar({onSearch}) {
+import {useState} from 'react';
+
+export default function SearchBar({onSearch}) {
+   const [id, setId] = useState('');
+
+   const handleChange = (event) =>  {
+   console.log(event);
+   setID(event.target.value)
+   }
+
    return (
       <div>
-         <input type='search' />
-         <button onClick={onSearch}>Agregar</button>
+         <input type='search' onChange={handleChange} value={id}/>
+         <button onClick={() => onSearch(id)}>Agregar</button>
       </div>
    );
 }
 
-export default SearchBar;
+
